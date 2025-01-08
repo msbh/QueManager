04bbc3ccf9c4f09db4a08d06249d5445bafcb975
Muhammad Misbah ul Haq Malik <malikmsbh@gmail.com>
2025-01-08 18:32:11 +0500

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,6 @@
 const initialState = {
   user: null,
+  users: [],  // Ensure users is an array from the beginning
   businesses: [],
   queues: {},
 };
@@ -8,12 +9,17 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_USER':
       return { ...state, user: action.payload };
+    
     case 'REGISTER_USER':
+      // Ensure users is an array and add the new user to it
       return { ...state, users: [...state.users, action.payload] };
+    
     case 'SET_BUSINESSES':
       return { ...state, businesses: action.payload };
+    
     case 'UPDATE_QUEUE':
       return { ...state, queues: { ...state.queues, ...action.payload } };
+    
     default:
       return state;
   }
